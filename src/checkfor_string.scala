cbee01ff1dda211001fb0b6e14117e2c28ca3411
rakeshrocky555 <rakeshkumargavara1@gmail.com>
2023-04-15 21:57:55 -0400

    Update checkfor_string.scala

--- src/checkfor_string.scala
+++ src/checkfor_string.scala
@@ -1,17 +1,25 @@
-object Solution {
+// object Solution {
     
-    def strStr(haystack: String, needle: String): Int = {
+//     def strStr(haystack: String, needle: String): Int = {
         
-        var first = 0
-        var index = -1
-        for(s <- 0 to (haystack.length() - needle.length())){
-            if(haystack.substring(s, s + needle.length()) == needle){
-                if(first == 0){
-                    index = s
-                    first = 1
-                }
-            }
-        }
-        index
+//         var first = 0
+//         var index = -1
+//         for(s <- 0 to (haystack.length() - needle.length())){
+//             if(haystack.substring(s, s + needle.length()) == needle){
+//                 if(first == 0){
+//                     index = s
+//                     first = 1
+//                 }
+//             }
+//         }
+//         index
+//     }
+// }
+
+
+import scala.util.matching.Regex
+object Solution {
+    def strStr(haystack: String, needle: String): Int = {
+        haystack.indexOf(needle, 0)
     }
 }
