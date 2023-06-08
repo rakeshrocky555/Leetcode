566ea6e46271cc05cbee31852a1374666f6ae0e2
Rakesh <rakeshkumargavara1@gmail.com>
2023-06-07 21:42:18 -0400

    Create stair_case.scala

---
+++ src/dynamic_programming/stair_case.scala
@@ -0,0 +1,22 @@
+object Solution {
+    def climbStairs(n: Int): Int = {
+        var res: Int = 0
+        var arr = new Array[Int](n+1)
+        for(i <- 0 to n){
+            arr(0) = 0
+        }
+        n match {
+                case value if(n == 0) => res = 0
+                case value if(n == 1) => res = 1
+                case _ => {
+                    arr(n-1) = 1
+                    arr(n) = 1
+                    for(i <- n-2 to 0 by -1){
+                        arr(i) = arr(i+1) + arr(i+2)
+                    }
+                    res = arr(0)
+                }
+        }
+    res
+    }
+}
