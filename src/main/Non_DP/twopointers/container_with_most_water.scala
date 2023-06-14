ca44fb24edf8d16a3c5579a90b327bdfa62ec06a
Rakesh <rakeshkumargavara1@gmail.com>
2023-06-14 18:57:55 -0400

    two pointers

--- src/main/Non_DP/twopointers/container_with_most_water.scala
+++ src/main/Non_DP/twopointers/container_with_most_water.scala
@@ -1,7 +1,8 @@
-package twopointers
+package Non_DP.twopointers
 
-import scala.math._
-object Solution_2 extends App {
+import scala.math.min
+
+object container_with_most_water extends App {
   def maxArea(height: Array[Int]): Int = {
 
     var l = 0
@@ -10,16 +11,16 @@ object Solution_2 extends App {
     var max_area = 0
 
     //iterate using two pointer technique
-    while(l < r){
-      temp_area = (r - l)*(min(height(l), height(r)))
-      if(temp_area > max_area){
+    while (l < r) {
+      temp_area = (r - l) * (min(height(l), height(r)))
+      if (temp_area > max_area) {
         max_area = temp_area
       }
       //exchange the heights
-      if(height(l) > height(r)){
+      if (height(l) > height(r)) {
         r = r - 1
       }
-      else{
+      else {
         l = l + 1
       }
     }
@@ -28,5 +29,5 @@ object Solution_2 extends App {
 
   }
 
-  println(maxArea(Array(1,8,6,2,5,4,8,3,7)))
+  println(maxArea(Array(1, 8, 6, 2, 5, 4, 8, 3, 7)))
 }
