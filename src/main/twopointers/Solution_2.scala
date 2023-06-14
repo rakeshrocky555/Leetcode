62b1481b1bb94dd6662925934972923b18bb0dc5
Rakesh <rakeshkumargavara1@gmail.com>
2023-06-14 18:55:24 -0400

    two pointers

---
+++ src/main/twopointers/Solution_2.scala
@@ -0,0 +1,32 @@
+package twopointers
+
+import scala.math._
+object Solution_2 extends App {
+  def maxArea(height: Array[Int]): Int = {
+
+    var l = 0
+    var r = height.length - 1
+    var temp_area = 0
+    var max_area = 0
+
+    //iterate using two pointer technique
+    while(l < r){
+      temp_area = (r - l)*(min(height(l), height(r)))
+      if(temp_area > max_area){
+        max_area = temp_area
+      }
+      //exchange the heights
+      if(height(l) > height(r)){
+        r = r - 1
+      }
+      else{
+        l = l + 1
+      }
+    }
+
+    max_area
+
+  }
+
+  println(maxArea(Array(1,8,6,2,5,4,8,3,7)))
+}
