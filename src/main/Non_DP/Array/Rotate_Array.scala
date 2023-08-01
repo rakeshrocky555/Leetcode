8289bc1699e80e29ec7b5f607d0304ae94a6175e
Rakesh <rakeshkumargavara1@gmail.com>
2023-08-01 18:44:35 -0400

    rotate array

---
+++ src/main/Non_DP/Array/Rotate_Array.scala
@@ -0,0 +1,23 @@
+package Non_DP.Array
+
+object Rotate_Array extends App{
+
+    def rotate(nums: Array[Int], k: Int): Unit = {
+
+      val steps = (k % (nums.length)).toInt
+      var nums1 = Array.ofDim[Int](nums.length)
+      for(i <- 0 to (nums.length - 1)){
+        nums1(i) = nums(i)
+      }
+      //nums1.foreach(println)
+
+      for(i <- 0 to (nums.length - 1)){
+        //nums1.foreach(print)
+        nums(((i + steps) % (nums.length))) = nums1(i)
+      }
+
+      nums.foreach(println)
+    }
+
+  println(rotate(Array(1,2,3,4,5,6,7), 3))
+  }
